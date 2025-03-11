7fd2eb240373c6ab6f215852b6e0c92b257df85f
LuizFRBZ <luizfernandobezerra0@gmail.com>
2025-03-11 11:21:56 -0300

    Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -2,53 +2,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Usuario extends Pessoa {
-    private Livro livroAlugado;
     private List<Item> itensAlugados = new ArrayList<>();
 
     public Usuario(String nome) {
         super(nome);
-        this.livroAlugado = null;
-    }
-
-    public Livro getLivroAlugado() {
-        return livroAlugado;
-    }
-
-    public void setLivroAlugado(Livro livroAlugado) {
-        this.livroAlugado = livroAlugado;
     }
 
     public void alugarItem(Item item) {
-        itensAlugados.add(item);
-        System.out.println(getNome() + " alugou o item: " + item.getTitulo());
-    }
-
-    public void devolverItem() {
         if (!itensAlugados.isEmpty()) {
-            itensAlugados.remove(itensAlugados.size() - 1);
-            System.out.println("Item devolvido com sucesso!");
+            System.out.println("Por favor, devolva o livro antes de alugar outro.");
+            return;
+        }
+
+        if (item.isDisponivel()) {
+            itensAlugados.add(item);
+            item.setDisponivel(false);
+            System.out.println(getNome() + " alugou o livro: " + item.getTitulo());
         } else {
-            System.out.println("Nenhum item para devolver!");
+            System.out.println("O livro " + item.getTitulo() + " não está disponível.");
         }
     }
 
-    public void alugarLivro(Livro livro) {
-        if (livro.isDisponivel()) {
-            livroAlugado = livro;
-            livro.setDisponivel(false);
-            System.out.println(getNome() + " alugou o livro: " + livro.getTitulo());
+    public void devolverItem(Item item) {
+        if (itensAlugados.contains(item)) {
+            itensAlugados.remove(item);
+            item.setDisponivel(true);
+            System.out.println(getNome() + " devolveu o livro: " + item.getTitulo());
         } else {
-            System.out.println("O livro " + livro.getTitulo() + " não está disponível.");
+            System.out.println(getNome() + " não tem este livro para devolver.");
         }
     }
 
-    public void devolverLivro() {
-        if (livroAlugado != null) {
-            livroAlugado.setDisponivel(true);
-            System.out.println(getNome() + " devolveu o livro: " + livroAlugado.getTitulo());
-            livroAlugado = null;
-        } else {
-            System.out.println(getNome() + " não tem nenhum livro para devolver.");
-        }
+    public boolean possuiLivroAlugado() {
+        return !itensAlugados.isEmpty();
     }
 }
